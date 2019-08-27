cd2284aba95b79a03f8949a631749c982f456193
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-08-27 08:56:18 +0200

    Add dynamic view

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.get('/first-template', function(req, res){
     res.render('first-template');
 });
 
+app.get('/dynamic-view', function(req, res){
+    res.render('dynamic', {
+        name: "Moja dynamiczna strona",
+        url: "http://www.google.com"
+    });
+});
+
 app.listen(3000);
 app.use(function (req, res, next) {
     res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
